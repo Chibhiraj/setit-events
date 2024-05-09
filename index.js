eaa6858e0c75e33429ffd705ce6bf101957bb901
Chibhiraj <77978640+Chibhiraj@users.noreply.github.com>
2024-05-09 23:39:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,79 +21,15 @@ dbConn.on("open", function(){
   console.log("DB Connection succesful");
 })
 
-const memberSchema= new mongoose.Schema({
-    designation: String,
-    location: String,    
-    address: String,
-    ownerName: String,
-    phoneNumber: String,
-    productLink: String,
-    shopName: String,
-});
 const EventSchema= new mongoose.Schema({
     eventName: String,
     eventLink: String
 });
 
-const user=mongoose.model('members',memberSchema);
 const event=mongoose.model('events',EventSchema);
 
+
 app.post('/', async (req, res) => {
-  try {
-    const { designation, location,address,ownerName,phoneNumber,productLink,shopName } = req.body;
-    const newUser = new user({ designation, location,address,ownerName,phoneNumber,productLink,shopName });
-    await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error) {
-    console.error("Error saving user:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.get('/', async (req, res) => {
-  try {
-    const users = await user.find({});
-    // console.log(users);
-    res.json(users);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.put('/:userId', async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const {designation, location,address,ownerName,phoneNumber,productLink,shopName } = req.body;
-    const updatedUser = await user.findByIdAndUpdate(userId, { designation, location,address,ownerName,phoneNumber,productLink,shopName }, { new: true });
-    if (!updatedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    console.error("Error updating user:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-
-app.delete('/:userId', async (req, res) => {
-  try {
-    const userId = req.params.userId; 
-    const deletedUser = await user.findByIdAndDelete(userId);
-    if (!deletedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.status(200).json({ message: "User deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting user:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-
-
-app.post('/event', async (req, res) => {
   try {
     const { eventName,eventLink } = req.body;
     const newEvent = new event({ eventName,eventLink });
@@ -105,7 +41,7 @@ app.post('/event', async (req, res) => {
   }
 });
 
-app.get('/event', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const events = await event.find({});
     // console.log(users);
@@ -116,7 +52,7 @@ app.get('/event', async (req, res) => {
   }
 });
 
-app.put('/event/:eventId', async (req, res) => {
+app.put('/:eventId', async (req, res) => {
   try {
     const eventId = req.params.eventId;
     const {event } = req.body;
@@ -132,7 +68,7 @@ app.put('/event/:eventId', async (req, res) => {
 });
 
 
-app.delete('/event/:eventId', async (req, res) => {
+app.delete('/:eventId', async (req, res) => {
   try {
     const eventId = req.params.eventId; 
     const deletedEvent = await event.findByIdAndDelete(userId);
